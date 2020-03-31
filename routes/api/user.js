e98cdf5d1d75f0ba0b67b08cf508b2ac5e3ea757
“Ronny <contact@ronnyperez.com>
2020-03-31 10:37:36 -0500

    added the login page and sign up page, css and routers.

---
+++ routes/api/user.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const userController = require("../../controllers/userController");
+
+
+router.route("/")
+  .get(userController.findAll)
+  .post(userController.create);
+
+  router.route("/login")
+  .post(userController.login);
+
+// Matches with "/api/whiskey/:id"
+router
+  .route("/:id")
+  .get(userController.findById)
+  .put(userController.update)
+  .delete(userController.remove);
+
+module.exports = router;
