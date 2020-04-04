2610f562640573c319cb3c8c27d61cf7f606a209
javyb92 <59591116+javyb92@users.noreply.github.com>
2020-04-04 09:37:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,6 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-var session = require('client-sessions');
-
-app.use(session({
-	cookieName: 'session',
-	secret: 'random_string_goes_here',
-	duration: 30 * 60 * 1000,
-	activeDuration: 5 * 60 * 1000,
-}));
-
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
